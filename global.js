81204a9ca3decb89f64c90c44df4403aed55667d
Xeom <fwharf.fw@googlemail.com>
2014-01-14 19:27:32 +0000

    mmmmm

--- global.js
+++ global.js
@@ -18,7 +18,24 @@ var subs = {
 	"Congressional Leaders": "river spirits",
 	"Homeland security": "Homestar runner",
 	"Could not be reached": "Is guilty",
-	"Could not be reached for comment": "Is guilty and everyone knows it"
+	"Could not be reached for comment": "Is guilty and everyone knows it",
+	"an Informed source": "My friend Alan",
+	"Programming": "Masturbating",
+	"Programmer": "Masturbator",
+	"Program": "Masturbate",
+	"Script": "Wank",
+	"Scripter": "Wanker",
+	"Webdev": "Wanker",
+	"Web developer": "Wanker",
+	"Scripting": "Wanking",
+	"Wank": "Script",
+	"Masturbating": "Programming",
+	"Masturbator": "Programmer",
+	"Wanker": "Webdev",
+	"Masturbate": "Program",
+	"Wanking": "Scripting",
+	"Continued": "Refused to stfu",
+	"Economist": "Grand mystic"
 }
 
 walk(document.body);
