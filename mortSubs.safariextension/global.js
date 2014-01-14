7d3e04c25b1498a4e47fc9e8c35906c2cc859e3a
Martin Dørum Nygaard <martid0311@gmail.com>
2014-01-14 19:19:33 +0100

    better structure, chrome version

---
+++ mortSubs.safariextension/global.js
@@ -0,0 +1,56 @@
+var subs = {
+	"Keyboard": "Leopard",
+	"The cloud": "My butt",
+	"Cloud": "Butt",
+	"Internet": "Interbutt",
+	"Witnesses": "These dudes I know",
+	"Allegedly": "Kinda probably",
+	"New study": "Tumblr post",
+	"Rebuild": "Avenge",
+	"Space": "Spaaace",
+	"Google Glasses": "Virtual Boy",
+	"Smartphone": "Pokédex",
+	"Electric": "Atomic",
+	"Senator": "Elf-lord",
+	"Senate": "Rivendell",
+	"Car": "Cat",
+	"Election": "Eating contest",
+	"Congressional Leaders": "river spirits",
+	"Homeland security": "Homestar runner",
+	"Could not be reached": "Is guilty",
+	"Could not be reached for comment": "Is guilty and everyone knows it"
+}
+
+walk(document.body);
+
+function walk(node) {
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	var child, next;
+	
+	switch (node.nodeType) {
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while (child) {
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+		case 3: // Text node
+			substitute(node);
+			break;
+	}
+}
+
+function substitute(textNode)  {
+	var v = textNode.nodeValue;
+	for (i in subs) {
+		v = v.replace(new RegExp("("+i+")"), subs[i]);
+		v = v.replace(new RegExp("("+i+")", "gi"), subs[i].toLowerCase());
+	}
+
+	textNode.nodeValue = v;
+}
